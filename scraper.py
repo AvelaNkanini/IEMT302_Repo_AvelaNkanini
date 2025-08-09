1e2b3757db9d399acd23402573841904ba058f2e
Avela <avelankanini745@gmail.com>
2025-08-10 01:36:37 +0200

    Add all required files with correct content and shebang

--- scraper.py
+++ scraper.py
@@ -4,9 +4,6 @@ import requests
 from bs4 import BeautifulSoup
 
 def main():
-    import requests
-    from bs4 import BeautifulSoup
-
     url = "https://wttr.in/Johannesburg?format=%C+%t"
     response = requests.get(url)
     print("Current Johannesburg Weather:", response.text.strip())
