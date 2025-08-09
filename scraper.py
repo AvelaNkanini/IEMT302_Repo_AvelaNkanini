a8884e872f35c1827d146d03257fc4b886f72484
Avela <avelankanini745@gmail.com>
2025-08-10 01:32:03 +0200

    Fix filenames, add shebang, add main function

--- scraper.py
+++ scraper.py
@@ -1,7 +1,12 @@
+#!/usr/bin/env python3
+
 import requests
 from bs4 import BeautifulSoup
 
 def main():
+    import requests
+    from bs4 import BeautifulSoup
+
     url = "https://wttr.in/Johannesburg?format=%C+%t"
     response = requests.get(url)
     print("Current Johannesburg Weather:", response.text.strip())
